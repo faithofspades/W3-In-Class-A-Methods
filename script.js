0ef1f6bc3cc99d2911528fc5cdebd5a54bdb5706
Faith <ardist456@gmail.com>
2025-01-27 13:50:37 -0500

    Update script.js
    made some methods

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //Let's make some objects again, but this time they can do stuff.
+const hat = { 
+    size : "large",
+    color : "blue",
+    onHead : false,
+    material : "corduroy",
+    adjustable : true,
+    brim : true,
+    sayHi : function() {
+        console.log(`I'm a ${this.size} ${this.color} ${this.material} hat`);
+    },
+    putOn : function() {
+        this.onHead = true;
+    },
+    takeOff : function() {
+        this.onHead = false;
+    }
+};
